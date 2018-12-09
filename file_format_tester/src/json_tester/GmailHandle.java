27264326874be0bb19acd1d93eeba6d99188c5c7
tinawh <tinawhuang95@gmail.com>
2018-12-09 10:43:46 -0500

    added parser

---
+++ file_format_tester/src/json_tester/GmailHandle.java
@@ -0,0 +1,5 @@
+package json_tester;
+
+public class GmailHandle {
+
+}
---
+++ file_format_tester/src/json_tester/Header.java
@@ -0,0 +1,33 @@
+package json_tester;
+
+/**
+ * Header Class models email headers
+ * @author tina
+ *
+ */
+public class Header {
+	String from;
+	String address;
+	
+	Header(String from, String address) {
+		this.from = from;
+		this.address = address;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
