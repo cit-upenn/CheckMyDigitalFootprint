517a207cb02baa9daf4d961a448581fda7858efa
WorthanKwan <wkk826@seas.upenn.edu>
2018-12-10 00:31:57 -0500

    Class for creating ascii project logo

---
+++ src/checkmydigitalfootprint/util/ProjectLogoPrinter.java
@@ -0,0 +1,50 @@
+
+public class ProjectLogoPrinter {
+	
+	public static void main(String[] args) {
+		System.out.println("mhsssssssyddddhhhddddddhhhhhdddddddddddddddddddddddddddddddddddddddddhyyyyyyyyyyyyyyyyyyyyyyyyyyyymN\r\n" + 
+				"d::::::/oyhysssssyyyhhsossssshhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhho+/////////////+++++++++++///sN\r\n" + 
+				"d::::/oyyyyyyyyyyyyyhs+osssssyhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhy+////////+++++++oooooooooo++sN\r\n" + 
+				"myyyyyyyyyyyysyyyyyhyooossssssyhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhy+///++++++++ooooooooooooooooyN\r\n" + 
+				"myyyyyyyyyyyyyyyyyhhsoosssssssyhhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhs//+++++++ooooooooooooooooooyN\r\n" + 
+				"myyyyyyyyyyyyyyyyhdyssssssssssshhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhy+/++oooooooooooooooooooooooyN\r\n" + 
+				"myyyyyyyyyyyyyyyydhssssssssssssyhhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhho/+ooooooooooooooooooooooooyN\r\n" + 
+				"myyyyyyyyyyyhdddddyssssssssssssyhhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhy++oooooooooooooooooooooooshN\r\n" + 
+				"myyyyyyyyyyyhdmmmmdysssssssyyyyyhdhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhhoooooooooooooooooooooooooshN\r\n" + 
+				"myyyyyyyyyyyyhdmNNmdyyyyyyhhhhhyhhhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhdyoossoossooooooooooooooosshN\r\n" + 
+				"myyyyyyyyyyyyhdmmmmy+/+osyhhhhhyhhdyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyydyoossssssssoooossoooooossshN\r\n" + 
+				"myyyyyyyyyyyyhyyhmmh/::::/+syhhhhhdhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhhsoooossooooooooooooooossyhN\r\n" + 
+				"myyyyyyyyyyyhhsosyddo:::::::/+syhhddyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhyooooooooooooooooossyyysshN\r\n" + 
+				"myyyyyyyyyyhhsooosydy+::::::::://oydhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhyooooooooooosssyyso+//:/:sN\r\n" + 
+				"myyyyyyyyyyhyooooosyhs/::::::::::::+syyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhhsooooosssyyso+//::::::::sN\r\n" + 
+				"myyyyyyyyyhhsossssssyho/:::::::::::::/oyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhhsssssso+/::::::::::::::sN\r\n" + 
+				"myyyyyyyyhdyyyyyyyysssys+:::::::::::::::+oyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyhhyso+/:::::::::::::::::/yN\r\n" + 
+				"myyyyyyyhdhyyyhhhhhhysyhyo/:::::::::::::::/+syyyyyyyyyyyyyyyyyyyyyyyyyssoo+/::::::::::::::::::::/odN\r\n" + 
+				"myyyyyyyddhhhhhhhhhhhhyhhyy+/:::::::::::::::://+++///////::::::::::::::::::::::::::::::::::::::/osdN\r\n" + 
+				"myyyyyyhdhhhhhhhhhhhhhhyhhhhs/::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::/+ossshN\r\n" + 
+				"mysyyyhdhhhhhhhhhhhhhhhhhhhhhyo/::::::::::::::::::::::::::::::::::::::::::::::::::::::::::/osyyyyyhN\r\n" + 
+				"mysyyyddhhhhhhhhhhhhhhhhhhhhhhhyo+++/::::::::::::::::::::::::::::::::::::::::::::::::::/+syyyhhhhhdN\r\n" + 
+				"myyyyhdhhhhhhhhhhhhhhhhhhhhhhhhhdyo/:::::::::::::::::::::::::::::::::::::::::::::////+osyyyyyhhhhhdN\r\n" + 
+				"myyyyddhhhhhhhhhhhhhhhhddddddhhhhs/:::::::::::::::::::::::::::::::::::::::::::::::/oyyyyyyyyyhhhhhdN\r\n" + 
+				"myyydddhhhhhhhhhhhhhhhdddmmddhhdy+:::::::::::::::::::::::::::::::::::::::::::::::::/syyyyyyyhhhhhhdN\r\n" + 
+				"myyhddhhhhhhhhhhhhhhhhddmmmdhhdho/:::::/++oso/::::::::::::::::::::/++oyys+::::::::::+syyyyyyyyyyyydN\r\n" + 
+				"myyddhhhhhhhhhhhhhhhhhhdhhhhyhdy/:::::/oo+oddy/:::::::::::::::::::/o+/ydds+::::::::::+yhyyyyyysyyyhN\r\n" + 
+				"myhmdhhhhdddddddddhhhhhhhhyyyhho::::::/ohhdmmh+:::::::::::::::::::/shdmmds+::::::::::/ohyssssssssshN\r\n" + 
+				"mhmmdhhdddmmmmmmmmmdhhhhhhyyyhy+::::::::+syyys/::::::::::::::::::::/+syys+::::::::::::/yyssssssssshN\r\n" + 
+				"mdmdhhddmmmmmmmmmmmdhhhyyyyyhho/::::::::::::::::::/+osso/:::::::::::::::::::::::::::::/syssssssssshN\r\n" + 
+				"mddhhhhddmmmmmmmmmmdhhyyyyyyddyoo+//:::::::::::::::/+oso+::::::::::::::::::///////:::::+yssssssssshN\r\n" + 
+				"mdhyhhhhddddhhhhhhhhyyyyyyyhddyyyyys+:::::::::::::::::::::::::::::::::::::+sssssso+/:::/sysssssssshN\r\n" + 
+				"mdhyhhhhhhyyyyyyyyyyyyyyyyhddysosssso/:::::::::::::::::::::::::::::::::::+ossssssss+::::+yyssssssshN\r\n" + 
+				"mdhhhhhhhhhhhhhhhyyyyyyyyydmdysssssso/:::::::::::/+osysssoo++/:::::::::::+ssssssssso/:::/ohyyyyyyyhN\r\n" + 
+				"mhhhhhhhhhhhhhhhhhhhhyyyyhddyyyyssso/::::::::::::+ydhso++++oo+/::::::::::/ossssssso/:::::+yyyyyyyyhN\r\n" + 
+				"msyyhhhhhhhhhhhhhhhhhhhhhhdho/+oo+//:::::::::::::+yyo+/////+oo/:::::::::::/+ooooo+/:::::::oyysssyyhN\r\n" + 
+				"msyhhhhhhhhhhhhhhhhhhhhhhhhhs/:::::::::::::::::::/oso+/////+oo/:::::::::::::::::::::::::::/yyssssshN\r\n" + 
+				"msyyyyyhhhhhhhhhyyhhhhhhhhhhho/::::::::::::::::::::+osoooooss+/:::::::::::::::::::::::::::/oysssyydN\r\n" + 
+				"myhhhyyysyyyysssssssssssssssyyo/:::::::::::::::::::::///++///::::::::::::::::::::::::::::::+yyyyyydN\r\n" + 
+				"mhhhhhyyyysssssssssssssssssyyyyo/::::::::::::::::::::::::::::::::::::::::::::::::::::::::::/shyyyydN\r\n" + 
+				"mhhhhhhhhhhhyyyysssssssssyyhhyys+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::oyhyyydN\r\n" + 
+				"myhhhhhhhhhhhhhhysssssssssssssyy+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::+yhyyydN\r\n" + 
+				"mdhhhhhddhhhhhhhhyyyyyyyyyyyyyhyo+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++oyhhhdmN");
+	}
+
+}
