4d5759fc7cb14f85366eaecbd92da42da5f7189d
Joseph <riverfish@gmail.com>
2018-12-12 18:03:04 -0500

    Add visual component test cases

--- src/test/java/ListServerOverviewControllerTest.java
+++ src/test/java/ListServerOverviewControllerTest.java
@@ -1,29 +1,47 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.concurrent.TimeoutException;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit.ApplicationTest;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXListView;
+
+import checkmydigitalfootprint.ListServerOverviewController;
 import checkmydigitalfootprint.MainApp;
+import checkmydigitalfootprint.model.ListServer;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class ListServerOverviewControllerTest extends ApplicationTest {
 	
 	Parent parent;
-
+	Label headerText;
+	Pane headerBox;
+	JFXButton scanButton;
+	JFXButton deleteButton;
+	JFXButton keepButton;
+	ObservableList<ListServer> list;
+	ListServerOverviewController controller;
+	MainApp mainApp;
+	
 	@Override
 	public void start(Stage stage) throws Exception {
-		parent = FXMLLoader.load(MainApp.class.getResource("FileUpload.fxml"));
+		parent = FXMLLoader.load(MainApp.class.getResource("ListServerOverview.fxml"));
 		stage.setScene(new Scene(parent));
 		stage.show();
 		stage.toFront();
@@ -31,7 +49,15 @@ public class ListServerOverviewControllerTest extends ApplicationTest {
 	
 	@Before
 	public void setUp() throws Exception {
+		headerText = find("#header-text");
+		headerBox = find("#header-box");
+		scanButton = find("#scan-btn");
+		deleteButton = find("#delete-btn");
+		keepButton = find("#keep-btn");
 		
+		controller = new ListServerOverviewController();
+		mainApp = new MainApp();
+		list = mainApp.getListServerList();
 	}
 	
 	@After
@@ -44,5 +70,61 @@ public class ListServerOverviewControllerTest extends ApplicationTest {
 	public <T extends Node> T find(final String query) {
 		return (T) lookup(query).queryAll().iterator().next();
 	}
-
+	
+	@Test
+	public void testHeaderText() {
+		final String text = "CheckMyDigitalFootprint";
+		assertEquals(text, headerText.getText());
+	}
+	
+	@Test
+	public void testHeaderBoxBackgroundColour() {
+		final String backgroundColour = "0x6d1b7bff";
+		assertEquals(backgroundColour, headerBox.getBackground().getFills().get(0).getFill().toString());
+	}
+	
+	@Test
+	public void testScanButtonText() {
+		final String text = "Scan";
+		assertEquals(text, scanButton.getText());
+	}
+	
+	@Test
+	public void testDeleteButtonText() {
+		final String text = "Delete List >>";
+		assertEquals(text, deleteButton.getText());
+	}
+	
+	@Test
+	public void testKeepButtonText() {
+		final String text = "<<  Keep List";
+		assertEquals(text, keepButton.getText());
+	}
+	
+	@Test
+	public void testScanButtonBackgroundColour() {
+		final String backgroundColour = "0x6d1b7bff";
+		assertEquals(backgroundColour, scanButton.getBackground().getFills().get(0).getFill().toString());
+	}
+	
+	@Test
+	public void testKeepButtonBackgroundColour() {
+		final String backgroundColour = "0x6d1b7bff";
+		assertEquals(backgroundColour, keepButton.getBackground().getFills().get(0).getFill().toString());
+	}
+	
+	@Test
+	public void testDeleteButtonBackgroundColour() {
+		final String backgroundColour = "0x6d1b7bff";
+		assertEquals(backgroundColour, deleteButton.getBackground().getFills().get(0).getFill().toString());
+	}
+	
+	@Test
+	public void testAddListServerToList() {
+		ListServer listServer = new ListServer("test", "test");
+		list.add(listServer);
+		assertEquals(1, list.size());
+	}
+	
+	
 }
